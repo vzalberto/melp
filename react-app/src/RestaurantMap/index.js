edb237f38bb51ee0feadfcf85bbd460a667879dd
Alberto <vzalberto@msn.com>
2019-10-27 17:59:44 -0600

    Add map click handler to update the global state

--- react-app/src/RestaurantMap/index.js
+++ react-app/src/RestaurantMap/index.js
@@ -1,30 +1,55 @@
-import React from 'react'
-import ReactMapGL from 'react-map-gl'
+import React, { useContext, useState } from 'react'
+import ReactMapGL, { NavigationControl } from 'react-map-gl'
 
 import 'mapbox-gl/dist/mapbox-gl.css'
 
 import {Box} from 'grommet'
 
-const viewport = {
+import Context from '../context'
+
+const navigationControl = {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    margin: "1em"
+  }
+
+const INITIAL_VIEWPORT = {
 	latitude: 37.7,
 	longitude: -122.45,
 	zoom: 13
 }
+const RestaurantMap = () => {
+	const {state, dispatch} = useContext(Context);
+	const [viewport, setViewport] = useState(INITIAL_VIEWPORT);
 
-const Map = () => {
+
+	const handleMapClick = ({lngLat, leftButton}) => {
+		if(!leftButton) return;
+		const [lng, lat] = lngLat;
+
+		dispatch ({type: "UPDATE_LONGITUDE" , payload: lng})
+
+		dispatch ({type: "UPDATE_LATITUDE", payload: lat})
+	}
+
 	return (
 		<Box pad="small" fill>
 			<ReactMapGL 
+				onClick={handleMapClick}
 				width="100vw"
 				height="100vh"
-				mapStyle="mapbox://styles/mapbox/streets-v9"
+				mapStyle="mapbox://styles/mapbox/satellite-v9"
 				{...viewport}
 				mapboxApiAccessToken = "pk.eyJ1IjoidnphbGJlcnRvIiwiYSI6ImNrMjllZzNjdTFtZXMzZXRjaHJ6Z2NlNW4ifQ.0DS64cOS3tH51tPaajudew"
+				onViewportChange={(viewport)=>setViewport(viewport)}
 			>
-
+			<div style={navigationControl}>
+			<NavigationControl onViewportChange={(viewport)=>setViewport(viewport)} />
+			</div>
 			</ReactMapGL>
 		</Box>
 	);
 }
 
-export default Map;
+export default RestaurantMap;
--- react-app/src/context.js
+++ react-app/src/context.js
@@ -3,7 +3,7 @@ import { createContext } from 'react';
 const Context = createContext({
 	lat: 19.4183786,
 	lng: -99.189842,
-	radius: 7000
+	radius: 7000,
 })
 
 export default Context;
